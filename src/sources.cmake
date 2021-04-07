e7448007af57e434a0e202f32202a11e1f7f60c2
Coale Cooper <coalecooper@gmail.com>
2021-04-07 16:59:07 -0700

    rename dataset generator

--- src/sources.cmake
+++ src/sources.cmake
@@ -1,7 +1,7 @@
 add_lab("ShadowRemoval")
 add_lab_solution("ShadowRemoval" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
 
-add_generator("Image" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
+add_generator("Image" ${CMAKE_CURRENT_LIST_DIR}/image_generator.cpp)
 
 add_lab("OtsuUnitTest")
 add_lab_solution("OtsuUnitTest" ${CMAKE_CURRENT_LIST_DIR}/otsu_method/unit_test.cu)
