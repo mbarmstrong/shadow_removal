0c1d37c0cdaf9f4d22179446b3b24010b65892e0
Coale Cooper <coalecooper@gmail.com>
2021-04-07 20:23:35 -0700

    add thresholding kernel and ut

--- src/otsu_method/unit_test.cu
+++ src/otsu_method/unit_test.cu
@@ -168,6 +168,61 @@ int main(void) {
   printf("%.5f, ", level);
   printf("\n\n");
 
+  float *hostInputImage;
+  float *deviceInputImage;
+  float *hostBinaryImage;
+  float *deviceBinaryImage;
+  int imageHeight = 2;
+  int imageWidth = 2;
+  int imageSize = imageHeight*imageWidth;
+
+  hostInputImage = (float *)malloc(imageSize * sizeof(float));
+  hostBinaryImage = (float *)malloc(imageSize * sizeof(float));
+
+  printf("\n\n\n non binary image:\t");
+  for(int i = 0; i < imageSize; i++){
+    hostInputImage[i] = float(i)*0.2f + 0.1f;
+    printf("%.5f, ", hostInputImage[i]); 
+  }
+  printf("\n\n");
+  
+  wbTime_start(GPU, "Allocating GPU memory.");
+  CUDA_CHECK( cudaMalloc((void **)&deviceInputImage, imageSize * sizeof(float)) );
+  CUDA_CHECK( cudaMalloc((void **)&deviceBinaryImage, imageSize * sizeof(float)) );
+  CUDA_CHECK( cudaDeviceSynchronize() );
+  wbTime_stop(GPU, "Allocating GPU memory.");
+
+  //@@ Copy memory to the GPU here
+  wbTime_start(GPU, "Copying input memory to the GPU.");
+  CUDA_CHECK(cudaMemcpy(deviceInputImage, hostInputImage,
+                        imageSize * sizeof(float),
+                        cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaDeviceSynchronize());
+  wbTime_stop(GPU, "Copying input memory to the GPU.");
+
+  // zero out bins
+  CUDA_CHECK(cudaMemset(deviceBinaryImage, 0.0, imageSize * sizeof(float)));
+
+  dim3 blockDim1(8,8), gridDim1(1,1);
+  create_binarized_image<<<gridDim1, blockDim1>>>(deviceInputImage, deviceBinaryImage,
+                                                level, imageWidth, imageHeight);
+  CUDA_CHECK(cudaGetLastError());
+  CUDA_CHECK(cudaDeviceSynchronize());
+
+  //@@ Copy the GPU memory back to the CPU here
+  wbTime_start(Copy, "Copying output memory to the CPU");
+  CUDA_CHECK(cudaMemcpy(hostBinaryImage, deviceBinaryImage,
+                        imageSize * sizeof(float),
+                        cudaMemcpyDeviceToHost));
+  CUDA_CHECK(cudaDeviceSynchronize());
+  wbTime_stop(Copy, "Copying output memory to the CPU");
+
+  printf("\n\nbinary image:\t");
+  for(int i = 0; i < imageSize; i++){
+      printf("%.5f, ", hostBinaryImage[i]);
+  }
+  printf("\n\n");
+
   //@@ Free the GPU memory here
   wbTime_start(GPU, "Freeing GPU Memory");
   CUDA_CHECK(cudaFree(deviceData));
@@ -175,6 +230,8 @@ int main(void) {
   CUDA_CHECK(cudaFree(deviceOmega));
   CUDA_CHECK(cudaFree(deviceMu));
   CUDA_CHECK(cudaFree(deviceSigmaBsq));
+  CUDA_CHECK(cudaFree(deviceInputImage));
+  CUDA_CHECK(cudaFree(deviceBinaryImage));
   wbTime_stop(GPU, "Freeing GPU Memory");
 
   return 0;
