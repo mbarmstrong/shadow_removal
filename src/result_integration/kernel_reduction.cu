9bc2a668bd2eae61c78dafe3b5def15e1ac102b7
Rituparna Dey <rdey1@login.localdomain>
2021-04-23 08:21:51 -0700

    Code clean up

--- src/result_integration/kernel_reduction.cu
+++ src/result_integration/kernel_reduction.cu
@@ -4,13 +4,9 @@
 
 #include "cuda_runtime.h"
 
-// Bandwidth: (((2^27) + 1) unsigned ints * 4 bytes/unsigned int)/(38.716 * 10^-3 s)
-//  13.867 GB/s = 96.297% -> excellent memory bandwidth
-// Reasonable point to stop working on this implementation's optimization
-// Algorithm is not compute-intensive, so acheiving >75% of theoretical bandwidth is goal
 // Main strategies used:
-// - Process as much data as possible (in terms of algorithm correctness) in shared memory
-// - Use sequential addressing to get rid of bank conflicts
+// Process as much data as possible (in terms of algorithm correctness) in shared memory
+// Use sequential addressing to get rid of bank conflicts
 __global__
 void block_sum_reduce(float* const d_block_sums, 
 	const float* const d_in,
@@ -23,8 +19,6 @@ void block_sum_reduce(float* const d_block_sums,
 	unsigned int tid = threadIdx.x;
 	
 	// Zero out shared memory
-	// Especially important when padding shmem for
-	//  non-power of 2 sized input
 	s_out[threadIdx.x] = 0;
 	s_out[threadIdx.x + blockDim.x] = 0;
 
@@ -64,8 +58,6 @@ void block_sum_reduce(float* const d_block_sums,
 	unsigned int tid = threadIdx.x;
 	
 	// Zero out shared memory
-	// Especially important when padding shmem for
-	//  non-power of 2 sized input
 	s_out[threadIdx.x] = 0;
 	s_out[threadIdx.x + blockDim.x] = 0;
 
@@ -93,13 +85,6 @@ void block_sum_reduce(float* const d_block_sums,
 		d_block_sums[blockIdx.x] = s_out[0];
 }
 
-
-// On my current laptop with GTX 850M, theoretical peak bandwidth is 14.4 GB/s
-// Shared memory of GTX 850M has 32 memory banks
-// Succeeding measurements are for the Release build
-
-// Bandwidth: (((2^27) + 1) unsigned ints * 4 bytes/unsigned int)/(173.444 * 10^-3 s)
-//  3.095 GB/s = 21.493% -> bad kernel memory bandwidth
 __global__ void reduce0(float* g_odata, float* g_idata, int len) {
 	extern __shared__ float sdata[];
 
@@ -134,8 +119,6 @@ __global__ void reduce0(float* g_odata, float* g_idata, int len) {
 }
 
 
-// Bandwidth: (((2^27) + 1) unsigned ints * 4 bytes/unsigned int)/(81.687 * 10^-3 s)
-//  6.572 GB/s = 45.639% -> bad kernel memory bandwidth, but better than last time
 __global__ void reduce1(float* g_odata, float* g_idata, int len) {
 	extern __shared__ float sdata[];
 
@@ -176,8 +159,7 @@ __global__ void reduce1(float* g_odata, float* g_idata, int len) {
 		g_odata[blockIdx.x] = sdata[0];
 }
 
-// Bandwidth: (((2^27) + 1) unsigned ints * 4 bytes/unsigned int)/(67.699 * 10^-3 s)
-//  7.930 GB/s = 55.069% -> good kernel memory bandwidth
+
 __global__ void reduce2(float* g_odata, float* g_idata, int len) {
 	extern __shared__ float sdata[];
 
@@ -211,8 +193,7 @@ __global__ void reduce2(float* g_odata, float* g_idata, int len) {
 		g_odata[blockIdx.x] = sdata[0];
 }
 
-// Bandwidth: (((2^27) + 1) unsigned ints * 4 bytes/unsigned int)/(58.505 * 10^-3 s)
-//  9.176 GB/s = 63.722% -> good kernel memory bandwidth, better than last time
+
 __global__ void reduce3(float* g_odata, float* g_idata, int len) {
 	extern __shared__ float sdata[];
 
@@ -234,8 +215,6 @@ __global__ void reduce3(float* g_odata, float* g_idata, int len) {
 
 	__syncthreads();
 
-	// do reduction in shared mem
-	// this loop now starts with s = 512 / 2 = 256
 	for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
 		if (tid < s) {
 			sdata[tid] += sdata[tid + s];
@@ -248,9 +227,6 @@ __global__ void reduce3(float* g_odata, float* g_idata, int len) {
 		g_odata[blockIdx.x] = sdata[0];
 }
 
-// Bandwidth: (((2^27) + 1) unsigned ints * 4 bytes/unsigned int)/(37.255 * 10^-3 s)
-//  14.411 GB/s = 100% -> perfect bandwidth? is this even possible?
-// ***In my laptop, measurements are wrong since the release version oddly outputs an incorrect value***
 __global__ void reduce4(float* g_odata, float* g_idata, int len) {
 	extern __shared__ float sdata[];
 
@@ -272,8 +248,7 @@ __global__ void reduce4(float* g_odata, float* g_idata, int len) {
 
 	__syncthreads();
 
-	// do reduction in shared mem
-	// this loop now starts with s = 512 / 2 = 256
+
 	for (unsigned int s = blockDim.x / 2; s > 32; s >>= 1) {
 		if (tid < s) {
 			sdata[tid] += sdata[tid + s];
@@ -313,15 +288,10 @@ float gpu_sum_reduce(float* d_in, int d_in_len)
 {
 	float total_sum = 0;
 
-	// Set up number of threads and blocks
-	// If input size is not power of two, the remainder will still need a whole block
-	// Thus, number of blocks must be the least number of 2048-blocks greater than the input size
 	int block_sz = MAX_BLOCK_SZ; // Halve the block size due to reduce3() and further 
 											  //  optimizations from there
 	// our block_sum_reduce()
 	int max_elems_per_block = block_sz * 2; // due to binary tree nature of algorithm
-	// NVIDIA's reduceX()
-	//unsigned int max_elems_per_block = block_sz;
 	
 	int grid_sz = 0;
 	if (d_in_len <= max_elems_per_block)
@@ -343,19 +313,13 @@ float gpu_sum_reduce(float* d_in, int d_in_len)
 
 	// Sum data allocated for each block
 	block_sum_reduce<<<grid_sz, block_sz, sizeof(float) * max_elems_per_block>>>(d_block_sums, d_in, d_in_len);
-	//reduce4<<<grid_sz, block_sz, sizeof(unsigned int) * block_sz>>>(d_block_sums, d_in, d_in_len);
-	//print_d_array(d_block_sums, grid_sz);
 
-	// Sum each block's total sums (to get global total sum)
-	// Use basic implementation if number of total sums is <= 2048
-	// Else, recurse on this same function
 	if (grid_sz <= max_elems_per_block)
 	{
 		float* d_total_sum;
 		CUDA_CHECK(cudaMalloc(&d_total_sum, sizeof(float)));
 		CUDA_CHECK(cudaMemset(d_total_sum, 0, sizeof(float)));
 		block_sum_reduce<<<1, block_sz, sizeof(float) * max_elems_per_block>>>(d_total_sum, d_block_sums, grid_sz);
-		//reduce4<<<1, block_sz, sizeof(float) * block_sz>>>(d_total_sum, d_block_sums, grid_sz);
 		CUDA_CHECK(cudaMemcpy(&total_sum, d_total_sum, sizeof(float), cudaMemcpyDeviceToHost));
 		CUDA_CHECK(cudaFree(d_total_sum));
 	}
@@ -377,15 +341,10 @@ float gpu_sum_reduce(unsigned char* d_in, int d_in_len)
 {
 	float total_sum = 0;
 
-	// Set up number of threads and blocks
-	// If input size is not power of two, the remainder will still need a whole block
-	// Thus, number of blocks must be the least number of 2048-blocks greater than the input size
 	int block_sz = MAX_BLOCK_SZ; // Halve the block size due to reduce3() and further 
 											  //  optimizations from there
 	// our block_sum_reduce()
 	int max_elems_per_block = block_sz * 2; // due to binary tree nature of algorithm
-	// NVIDIA's reduceX()
-	//unsigned int max_elems_per_block = block_sz;
 	
 	int grid_sz = 0;
 	if (d_in_len <= max_elems_per_block)
@@ -407,19 +366,13 @@ float gpu_sum_reduce(unsigned char* d_in, int d_in_len)
 
 	// Sum data allocated for each block
 	block_sum_reduce<<<grid_sz, block_sz, sizeof(float) * max_elems_per_block>>>(d_block_sums, d_in, d_in_len);
-	//reduce4<<<grid_sz, block_sz, sizeof(unsigned int) * block_sz>>>(d_block_sums, d_in, d_in_len);
-	//print_d_array(d_block_sums, grid_sz);
 
-	// Sum each block's total sums (to get global total sum)
-	// Use basic implementation if number of total sums is <= 2048
-	// Else, recurse on this same function
 	if (grid_sz <= max_elems_per_block)
 	{
 		float* d_total_sum;
 		CUDA_CHECK(cudaMalloc(&d_total_sum, sizeof(float)));
 		CUDA_CHECK(cudaMemset(d_total_sum, 0, sizeof(float)));
 		block_sum_reduce<<<1, block_sz, sizeof(float) * max_elems_per_block>>>(d_total_sum, d_block_sums, grid_sz);
-		//reduce4<<<1, block_sz, sizeof(float) * block_sz>>>(d_total_sum, d_block_sums, grid_sz);
 		CUDA_CHECK(cudaMemcpy(&total_sum, d_total_sum, sizeof(float), cudaMemcpyDeviceToHost));
 		CUDA_CHECK(cudaFree(d_total_sum));
 	}
