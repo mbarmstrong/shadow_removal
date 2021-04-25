8f308dbd083ba2dd5b4152cb75d5d9c54c01abd1
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-04-25 16:00:00 -0700

    timing erosion

--- src/erosion/unit_test.cu
+++ src/erosion/unit_test.cu
@@ -118,7 +118,7 @@ int main(int argc, char *argv[]) {
     printf("\nRunning erosion unit test on image of %dx%d\n",
              imageWidth, imageHeight, NUM_CHANNELS);
 
-    // inputImage_RGB_uint8 = (unsigned char*)malloc(imageSize * sizeof(unsigned char));
+    inputImage_RGB_uint8 = (unsigned char*)malloc(imageSize * sizeof(unsigned char));
 
     // for(int i = 0; i < imageSize; i++){
     //     inputImage_RGB_uint8[i] = (unsigned char)(round(wbImage_getData(inputImage_RGB)[i*3]));
