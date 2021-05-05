62af665b2675ad2d2ac4415528006dc29df06d83
Rituparna Dey <rdey1@login.localdomain>
2021-05-04 21:07:10 -0700

    Code cleanup

--- src/result_integration/kernel.cu
+++ src/result_integration/kernel.cu
@@ -1,5 +1,6 @@
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
+
 // Kernel 1: Finding average channel values in shadow/light areas for every channel
 __global__ void multiply_rgbImage_byMask(float *rgbImage, unsigned char *greyShadowMask, 
   unsigned char *greyLightMask, float *redShadowArray,float *greenShadowArray,float *blueShadowArray,
@@ -51,87 +52,8 @@ __global__ void multiply_rgbImage_byMask(float *rgbImage, unsigned char *greySha
   
   }
 
- // Kernel 2: Array Reduction Kernel - 1D array
-  __global__ void sum_up_arrays_by_reduction(float *g_idata, float *g_odata, int n) {
-    extern __shared__ float sdata[];
 
-    // each thread loads one element from global to shared mem
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-    int tid = threadIdx.x;
-
-    sdata[tid] = g_idata[i];
-
-    // do reduction in shared mem
-    for (int s=1; s < blockDim.x; s *=2)
-    {
-      __syncthreads();
-        int index = 2 * s * tid;
-
-        if (index < blockDim.x)
-        {
-            sdata[index] += sdata[index + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) {
-        g_odata[blockIdx.x] = sdata[tid];
-    }
-
-}
-
- // Kernel 2: Array Reduction Kernel - 1D array
-  __global__ void sum_up_arrays_by_reduction1(unsigned char *g_idata, float *g_odata, int n) {
-    extern __shared__ float sdata[];
-
-    // each thread loads one element from global to shared mem
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-    int tid = threadIdx.x;
-
-    sdata[tid] = g_idata[i];
-
-    // do reduction in shared mem
-    for (int s=1; s < blockDim.x; s *=2)
-    {
-      __syncthreads();
-        int index = 2 * s * tid;
-
-        if (index < blockDim.x)
-        {
-            sdata[index] += sdata[index + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) {
-        g_odata[blockIdx.x] = sdata[tid];
-    }
-
-}
-
-  __global__ void sum_up_arrays_naive(float *g_idata, float *g_odata, int n,int width,int height) {
-
-    int col = threadIdx.x + blockIdx.x * blockDim.x; // column index
-    int row = threadIdx.y + blockIdx.y * blockDim.y; // row index
-  
-    if (col < width && row < height) {  // check boundary condition
-        int idx = row * width + col;      // mapping 2D to 1D coordinate
-        g_odata[idx] = g_odata[idx] + g_idata[idx];
-       
-    }
-    __syncthreads();
-}
-
-  float sum_up_arrays_thrust(float *g_idata,int imageSize) {
-
-    thrust::device_vector<float> deviceInput(g_idata,g_idata+imageSize);
-    float outputSum = thrust::reduce(deviceInput.begin(),deviceInput.end());
-    return outputSum;
-}
-
-  // Calculates the Red, Green and Blue ratios from the sum in Kernel 2 to produce the shadowless image 
+  // Kernel 3 - Calculates the Red, Green and Blue ratios from the sum in Kernel 2 to produce the shadowless image 
   // Uses the RGB ratios produced in Kernel 3 and the input image to remove the shadow and create the final output 
 
   __global__ void calculate_final_image(float *redSumShadowArray, float *greenSumShadowArray,float *blueSumShadowArray,
@@ -160,7 +82,8 @@ __global__ void multiply_rgbImage_byMask(float *rgbImage, unsigned char *greySha
     }
   }
 
-    __global__ void calculate_final_image_optimised(float redRatio, float greenRatio,float blueRatio,
+  //Kernel3 - Optimized the kernel by moving the ratio calculation to host code and passing the ratios as float
+  __global__ void calculate_final_image_optimised(float redRatio, float greenRatio,float blueRatio,
     float *rgbImage, float *smoothMask, float *finalImage,
     int width, int height, int numChannels) {
   
@@ -183,55 +106,3 @@ __global__ void multiply_rgbImage_byMask(float *rgbImage, unsigned char *greySha
         finalImage[blueIdx] = ((blueRatio + 1) / ((1 - smoothMask[idx]) * blueRatio + 1) * blue);
     }
   }
-
-    __global__ void calculate_final_image_optimised(float *redRatio, float *greenRatio,float *blueRatio,
-    float *rgbImage, float *smoothMask, float *finalImage,
-    int width, int height, int numChannels) {
-  
-    int col = threadIdx.x + blockIdx.x * blockDim.x; // column index
-    int row = threadIdx.y + blockIdx.y * blockDim.y; // row index
-  
-    if (col < width && row < height) {  // check boundary condition
-        int idx = row * width + col;      // mapping 2D to 1D coordinate
-        // load input RGB values
-        int redIdx = numChannels * idx;
-        int greenIdx = numChannels * idx + 1;
-        int blueIdx = numChannels * idx + 2;
-
-        float red = rgbImage[redIdx];      // red component
-        float green = rgbImage[greenIdx];  // green component
-        float blue = rgbImage[blueIdx];  // blue component
-
-        finalImage[redIdx] = ((redRatio[0] + 1) / ((1 - smoothMask[idx]) * redRatio[0] + 1) * red);
-        finalImage[greenIdx] = ((greenRatio[0] + 1) / ((1 - smoothMask[idx]) * greenRatio[0] + 1) * green);
-        finalImage[blueIdx] = ((blueRatio[0] + 1) / ((1 - smoothMask[idx]) * blueRatio[0] + 1) * blue);
-    }
-  }
-
-    __global__ void calculate_final_image_stride(float redRatio, float greenRatio,float blueRatio,
-    float *rgbImage, float *smoothMask, float *finalImage,
-    int width, int height, int numChannels) {
-
-    int col = threadIdx.x + blockIdx.x * blockDim.x; // column index
-    int row = threadIdx.y + blockIdx.y * blockDim.y; // row index
-  
-    if (col < width && row < height) {  // check boundary condition
-        int idx = row * width + col;      // mapping 2D to 1D coordinate
-        // load input RGB values
-        int redIdx = numChannels * idx;
-        int greenIdx = numChannels * idx + 1;
-        int blueIdx = numChannels * idx + 2;
-
-        float red = rgbImage[redIdx];      // red component
-        float green = rgbImage[greenIdx];  // green component
-        float blue = rgbImage[blueIdx];  // blue component
-
-        float finalImageRed = ((redRatio + 1) / ((1 - smoothMask[idx]) * redRatio + 1) * red);
-        float finalImageGreen = ((greenRatio + 1) / ((1 - smoothMask[idx]) * greenRatio + 1) * green);
-        float finalImageBlue = ((blueRatio + 1) / ((1 - smoothMask[idx]) * blueRatio + 1) * blue);
-
-        finalImage[redIdx] = finalImageRed;
-        finalImage[greenIdx] = finalImageGreen;
-        finalImage[blueIdx] = finalImageBlue;
-    }
-    }
--- src/result_integration/kernel_reduction.cu
+++ src/result_integration/kernel_reduction.cu
@@ -1,154 +1,16 @@
 #include "../globals.h"
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
 
-#include <cmath>
+//Naive approach to perform array reduction using thrust library
+float sum_up_arrays_thrust(float *g_idata,int imageSize) {
 
-#include "cuda_runtime.h"
-
-// Main strategies used:
-// Process as much data as possible (in terms of algorithm correctness) in shared memory
-// Use sequential addressing to get rid of bank conflicts
-__global__
-void block_sum_reduce(float* const d_block_sums, 
-    const float* const d_in,
-    const int d_in_len)
-{
-    extern __shared__ float s_out[];
-
-    unsigned int glbl_tid = blockDim.x * blockIdx.x + threadIdx.x;
-    unsigned int tid = threadIdx.x;
-
-    // Zero out shared memory
-    s_out[threadIdx.x] = 0;
-    s_out[threadIdx.x + blockDim.x] = 0;
-
-    __syncthreads();
-
-    // Copy d_in to shared memory per block
-    if (glbl_tid < d_in_len)
-    {
-        s_out[threadIdx.x] = (float)d_in[glbl_tid];
-        if (glbl_tid + blockDim.x < d_in_len)
-            s_out[threadIdx.x + blockDim.x] = (float)d_in[glbl_tid + blockDim.x];
-    }
-    __syncthreads();
-
-    // Actually do the reduction
-    for (unsigned int s = blockDim.x; s > 0; s >>= 1) {
-        if (tid < s) {
-            s_out[tid] += s_out[tid + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0)
-        d_block_sums[blockIdx.x] = s_out[0];
+    thrust::device_vector<float> deviceInput(g_idata,g_idata+imageSize);
+    float outputSum = thrust::reduce(deviceInput.begin(),deviceInput.end());
+    return outputSum;
 }
 
-__global__
-void block_sum_reduce(float* const d_block_sums, 
-    const unsigned char* const d_in,
-    const int d_in_len)
-{
-    extern __shared__ float s_out[];
-
-    unsigned int glbl_tid = blockDim.x * blockIdx.x + threadIdx.x;
-    unsigned int tid = threadIdx.x;
-    
-    // Zero out shared memory
-    s_out[threadIdx.x] = 0.0;
-    s_out[threadIdx.x + blockDim.x] = 0.0;
-
-    __syncthreads();
-
-    // Copy d_in to shared memory per block
-    if (glbl_tid < d_in_len)
-    {
-        s_out[threadIdx.x] = (float)d_in[glbl_tid];
-        if (glbl_tid + blockDim.x < d_in_len)
-            s_out[threadIdx.x + blockDim.x] = (float)d_in[glbl_tid + blockDim.x];
-    }
-    __syncthreads();
-
-    // Actually do the reduction
-    for (unsigned int s = blockDim.x; s > 0; s >>= 1) {
-        if (tid < s) {
-            s_out[tid] += s_out[tid + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0)
-        d_block_sums[blockIdx.x] = s_out[0];
-}
-
-__global__ void reduce0(float* g_odata, float* g_idata, int len) {
-    extern __shared__ float sdata[];
-
-    // each thread loads one element from global to shared mem
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-
-    sdata[tid] = 0;
-
-    if (i < len)
-    {
-        sdata[tid] = g_idata[i];
-    }
-
-    __syncthreads();
-
-    // do reduction in shared mem
-    // Interleaved addressing, which causes huge thread divergence
-    //  because threads are active/inactive according to their thread IDs
-    //  being powers of two. The if conditional here is guaranteed to diverge
-    //  threads within a warp.
-    for (unsigned int s = 1; s < 2048; s <<= 2) {
-        if (tid % (2 * s) == 0) {
-            sdata[tid] += sdata[tid + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) 
-        g_odata[blockIdx.x] = sdata[0];
-}
-
-__global__ void reduce0(float* g_odata, unsigned char* g_idata, int len) {
-    extern __shared__ float sdata[];
-
-    // each thread loads one element from global to shared mem
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-
-    sdata[tid] = 0.0;
-
-    if (i < len)
-    {
-        sdata[tid] = (float) g_idata[i];
-    }
-
-    __syncthreads();
-
-    // do reduction in shared mem
-    // Interleaved addressing, which causes huge thread divergence
-    //  because threads are active/inactive according to their thread IDs
-    //  being powers of two. The if conditional here is guaranteed to diverge
-    //  threads within a warp.
-    for (unsigned int s = 1; s < 2048; s <<= 2) {
-        if (tid % (2 * s) == 0) {
-            sdata[tid] += sdata[tid + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) 
-        g_odata[blockIdx.x] = sdata[0];
-}
-
+//Array Reduction using Interleaved Addressing
 __global__ void reduce1(float* g_odata, float* g_idata, int len) {
     extern __shared__ float sdata[];
 
@@ -189,46 +51,8 @@ __global__ void reduce1(float* g_odata, float* g_idata, int len) {
         g_odata[blockIdx.x] = sdata[0];
 }
 
-__global__ void reduce1(float* g_odata, unsigned char* g_idata, int len) {
-    extern __shared__ float sdata[];
 
-    // each thread loads one element from global to shared mem
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-
-    sdata[tid] = 0.0;
-
-    if (i < len)
-    {
-        sdata[tid] = (float) g_idata[i];
-    }
-
-    __syncthreads();
-
-    // do reduction in shared mem
-    // Interleaved addressing, but threads being active/inactive
-    //  is no longer based on thread IDs being powers of two. Consecutive
-    //  threadIDs now run, and thus solves the thread diverging issue within
-    //  a warp
-    // However, this introduces shared memory bank conflicts, as threads start 
-    //  out addressing with a stride of two 32-bit words (unsigned ints),
-    //  and further increase the stride as the current power of two grows larger
-    //  (which can worsen or lessen bank conflicts, depending on the amount
-    //  of stride)
-    for (unsigned int s = 1; s < blockDim.x; s *= 2) {
-        unsigned int index = 2 * s * tid;
-
-        if (index < blockDim.x) {
-            sdata[index] += sdata[index + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0)
-        g_odata[blockIdx.x] = sdata[0];
-}
-
+//Array Reduction using Sequential Addressing to resolve Bank conflicts
 __global__ void reduce2(float* g_odata, float* g_idata, int len) {
     extern __shared__ float sdata[];
 
@@ -261,43 +85,12 @@ __global__ void reduce2(float* g_odata, float* g_idata, int len) {
     if (tid == 0)
         g_odata[blockIdx.x] = sdata[0];
 }
-__global__ void reduce2(float* g_odata, unsigned char* g_idata, int len) {
-    extern __shared__ float sdata[];
 
-    // each thread loads one element from global to shared mem
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-
-    sdata[tid] = 0.0;
-
-    if (i < len)
-    {
-        sdata[tid] = (float) g_idata[i];
-    }
-
-    __syncthreads();
-
-    // do reduction in shared mem
-    // Sequential addressing. This solves the bank conflicts as
-    //  the threads now access shared memory with a stride of one
-    //  32-bit word (unsigned int) now, which does not cause bank 
-    //  conflicts
-    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
-        if (tid < s) {
-            sdata[tid] += sdata[tid + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0)
-        g_odata[blockIdx.x] = sdata[0];
-}
-
+//Array Reduction First Add during Load approach. Block size is halved.
 __global__ void reduce3(float* g_odata, float* g_idata, int len) {
     extern __shared__ float sdata[];
 
-    // each thread loads one element from global to shared mem
+    // Each thread loads one element from global to shared mem
     // Do the first stage of the reduction on the global-to-shared load step
     // This reduces the previous inefficiency of having half of the threads being
     //  inactive on the first for-loop iteration below (previous first step of reduction)
@@ -327,39 +120,7 @@ __global__ void reduce3(float* g_odata, float* g_idata, int len) {
         g_odata[blockIdx.x] = sdata[0];
 }
 
-__global__ void reduce3(float* g_odata, unsigned char* g_idata, int len) {
-    extern __shared__ float sdata[];
-
-    // each thread loads one element from global to shared mem
-    // Do the first stage of the reduction on the global-to-shared load step
-    // This reduces the previous inefficiency of having half of the threads being
-    //  inactive on the first for-loop iteration below (previous first step of reduction)
-    // Previously, only less than or equal to 512 out of 1024 threads in a block are active.
-    // Now, all 512 threads in a block are active from the start
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
-
-    sdata[tid] = 0.0;
-
-    if (i < len)
-    {
-        sdata[tid] = (float)g_idata[i] + (float)g_idata[i + blockDim.x];
-    }
-
-    __syncthreads();
-
-    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
-        if (tid < s) {
-            sdata[tid] += sdata[tid + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0)
-        g_odata[blockIdx.x] = sdata[0];
-}
-
+//Array Reduction using Unrolling of Last Loops
 __global__ void reduce4(float* g_odata, float* g_idata, int len) {
     extern __shared__ float sdata[DIM];
 
@@ -404,63 +165,7 @@ __global__ void reduce4(float* g_odata, float* g_idata, int len) {
         g_odata[blockIdx.x] = sdata[0];
 }
 
-__global__ void reduce4(float* g_odata, unsigned char* g_idata, int len) {
-    extern __shared__ float sdata[];
-
-    // each thread loads one element from global to shared mem
-    // Do the first stage of the reduction on the global-to-shared load step
-    // This reduces the previous inefficiency of having half of the threads being
-    //  inactive on the first for-loop iteration below (previous first step of reduction)
-    // Previously, only less than or equal to 512 out of 1024 threads in a block are active.
-    // Now, all 512 threads in a block are active from the start
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
-
-    sdata[tid] = 0.0;
-
-    if (i < len)
-    {
-        sdata[tid] = (float)g_idata[i] + (float)g_idata[i + blockDim.x];
-    }
-
-    __syncthreads();
-
-
-    for (unsigned int s = blockDim.x / 2; s > 32; s >>= 1) {
-        if (tid < s) {
-            sdata[tid] += sdata[tid + s];
-        }
-        __syncthreads();
-    }
-
-    if (tid < 32)
-    {
-        sdata[tid] += sdata[tid + 32];
-        sdata[tid] += sdata[tid + 16];
-        sdata[tid] += sdata[tid + 8];
-        sdata[tid] += sdata[tid + 4];
-        sdata[tid] += sdata[tid + 2];
-        sdata[tid] += sdata[tid + 1];
-    }
-
-    // write result for this block to global mem
-    if (tid == 0)
-        g_odata[blockIdx.x] = sdata[0];
-}
-
-void print_d_array(float* d_array, int len)
-{
-    float* h_array = new float[len];
-    CUDA_CHECK(cudaMemcpy(h_array, d_array, sizeof(float) * len, cudaMemcpyDeviceToHost));
-    for (int i = 0; i < len; ++i)
-    {
-        std::cout << h_array[i] << " ";
-    }
-    std::cout << std::endl;
-
-    delete[] h_array;
-}
-
+//Method to invoke the reduction kernel recursively
 float gpu_sum_reduce(float* d_in, int d_in_len)
 {
 
@@ -489,14 +194,12 @@ float gpu_sum_reduce(float* d_in, int d_in_len)
     CUDA_CHECK(cudaMemset(d_block_sums, 0.0, sizeof(float) * grid_sz));
 
     // Sum data allocated for each block
-    //block_sum_reduce<<<grid_sz, block_sz, sizeof(float) * max_elems_per_block>>>(d_block_sums, d_in, d_in_len);
     reduce4<<<grid_sz, block_sz, sizeof(float) * max_elems_per_block>>>(d_block_sums, d_in, d_in_len);
     if (grid_sz <= max_elems_per_block)
     {
         float* d_total_sum;
         CUDA_CHECK(cudaMalloc(&d_total_sum, sizeof(float)));
         CUDA_CHECK(cudaMemset(d_total_sum, 0.0, sizeof(float)));
-        //block_sum_reduce<<<1, block_sz, sizeof(float) * max_elems_per_block>>>(d_total_sum, d_block_sums, grid_sz);
         reduce4<<<1, block_sz, sizeof(float) * max_elems_per_block>>>(d_total_sum, d_block_sums, grid_sz);
         CUDA_CHECK(cudaMemcpy(&total_sum, d_total_sum, sizeof(float), cudaMemcpyDeviceToHost));
         CUDA_CHECK(cudaFree(d_total_sum));
@@ -512,202 +215,4 @@ float gpu_sum_reduce(float* d_in, int d_in_len)
 
     CUDA_CHECK(cudaFree(d_block_sums));
     return total_sum;
-}
-
-
-float gpu_sum_reduce(unsigned char* d_in, int d_in_len)
-{
-    float total_sum = 0;
-
-    int block_sz = MAX_BLOCK_SZ; // Halve the block size due to reduce3() and further 
-                                              //  optimizations from there
-    // our block_sum_reduce()
-    int max_elems_per_block = block_sz * 2; // due to binary tree nature of algorithm
-    
-    int grid_sz = 0;
-    if (d_in_len <= max_elems_per_block)
-    {
-        // grid_sz = (int)std::ceil(float(d_in_len) / float(max_elems_per_block));
-        grid_sz = (int)std::ceil(float(d_in_len) / float(max_elems_per_block));
-    }
-    else
-    {
-        // grid_sz = d_in_len / max_elems_per_block;
-        grid_sz = d_in_len / max_elems_per_block;
-        if (d_in_len % block_sz != 0)
-            grid_sz++;
-    }
-    // Allocate memory for array of total sums produced by each block
-    // Array length must be the same as number of blocks / grid size
-    float* d_block_sums;
-    CUDA_CHECK(cudaMalloc(&d_block_sums, sizeof(float) * grid_sz));
-    CUDA_CHECK(cudaMemset(d_block_sums, 0, sizeof(float) * grid_sz));
-    // Sum data allocated for each block
-    reduce4<<<grid_sz, block_sz, sizeof(float) * max_elems_per_block>>>(d_block_sums, d_in, d_in_len);
-    //block_sum_reduce<<<grid_sz, block_sz, sizeof(float) * max_elems_per_block>>>(d_block_sums, d_in, d_in_len);
-
-    if (grid_sz <= max_elems_per_block)
-    {
-        float* d_total_sum;
-        CUDA_CHECK(cudaMalloc(&d_total_sum, sizeof(float)));
-        CUDA_CHECK(cudaMemset(d_total_sum, 0, sizeof(float)));
-        //block_sum_reduce<<<1, block_sz, sizeof(float) * max_elems_per_block>>>(d_total_sum, d_block_sums, grid_sz);
-        reduce4<<<1, block_sz, sizeof(float) * max_elems_per_block>>>(d_total_sum, d_block_sums, grid_sz);
-        CUDA_CHECK(cudaMemcpy(&total_sum, d_total_sum, sizeof(float), cudaMemcpyDeviceToHost));
-        CUDA_CHECK(cudaFree(d_total_sum));
-    }
-    else
-    {
-        float* d_in_block_sums;
-        CUDA_CHECK(cudaMalloc(&d_in_block_sums, sizeof(float) * grid_sz));
-        CUDA_CHECK(cudaMemcpy(d_in_block_sums, d_block_sums, sizeof(float) * grid_sz, cudaMemcpyDeviceToDevice));
-        total_sum = gpu_sum_reduce(d_in_block_sums, grid_sz);
-        CUDA_CHECK(cudaFree(d_in_block_sums));
-    }
-
-    CUDA_CHECK(cudaFree(d_block_sums));
-    return total_sum;
-}
-
-
-__global__ void reductionFloat(float *vect, float *vecOut, int size)
-{
-	extern __shared__ float block[];
-	unsigned int globalIndex = threadIdx.x + blockDim.x*blockIdx.x;
-	unsigned int i = threadIdx.x;
-	if (globalIndex < size)
-	{
-		block[i] = vect[globalIndex];
-	}	
-	else
-	{
-		block[i] = 0;
-	}	
-
-	__syncthreads();
-
-	for (unsigned int j = blockDim.x / 2; j > 32; j >>= 1)
-	{
-		if (i < j)
-			block[i] += block[i + j];
-
-		__syncthreads();
-	}
-
-	if (i < 32)
-	{
-		block[i] += block[i + 32];
-		block[i] += block[i + 16];
-		block[i] += block[i + 8];
-		block[i] += block[i + 4];
-		block[i] += block[i + 2];
-		block[i] += block[i + 1];
-	}
-	if (i == 0)
-		vecOut[blockIdx.x] = block[0];
-}
-
-__global__ void reductionUnsignedChar(unsigned char *vect, unsigned char *vecOut, int size)
-{
-	extern __shared__ unsigned char block1[];
-	unsigned int globalIndex = threadIdx.x + blockDim.x*blockIdx.x;
-	unsigned int i = threadIdx.x;
-	if (globalIndex < size)
-	{	block1[i] = vect[globalIndex];
-	}
-	else
-	{
-		block1[i] = 0;
-	}	
-
-	__syncthreads();
-
-	for (unsigned int j = blockDim.x / 2; j > 0; j >>= 1)
-	{
-		if (i < j)
-			block1[i] += block1[i + j];
-
-		__syncthreads();
-	}
-
-	if (i < 32)
-	{
-		block1[i] += block1[i + 32];
-		block1[i] += block1[i + 16];
-		block1[i] += block1[i + 8];
-		block1[i] += block1[i + 4];
-		block1[i] += block1[i + 2];
-		block1[i] += block1[i + 1];
-	}
-	if (i == 0)
-		vecOut[blockIdx.x] = block1[0];
-}
-
-
-void sumGPUFloat(float *g_idata, float *g_odata, int size)
-{
-	int numInputElements = size;
-	int numOutputElements;
-	int threadsPerBlock = DIM;
-	float *dev_g_idata;
-	float *dev_g_odata;
-	cudaSetDevice(0);
-	cudaMalloc((float**)&dev_g_idata, size * sizeof(float));
-	cudaMalloc((float**)&dev_g_odata, size * sizeof(float));
-	cudaMemcpy(dev_g_idata, g_idata, size * sizeof(float), cudaMemcpyHostToDevice);
-
-	do
-	{
-		numOutputElements = numInputElements / (threadsPerBlock);
-		if (numInputElements % (threadsPerBlock))
-			numOutputElements++;
-
-		reductionFloat <<< numOutputElements, threadsPerBlock ,DIM * sizeof(float)>>> (dev_g_idata, dev_g_odata, numInputElements);
-
-		numInputElements = numOutputElements;
-		if (numOutputElements > 1)
-		{
-			reductionFloat <<< numOutputElements, threadsPerBlock ,DIM * sizeof(float)>>>  (dev_g_odata, dev_g_idata, numInputElements);
-		}
-	} while (numOutputElements > 1);
-
-	cudaDeviceSynchronize();
-	cudaMemcpy(g_idata, dev_g_idata, size * sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(g_odata, dev_g_odata, size * sizeof(float), cudaMemcpyDeviceToHost);
-
-	cudaFree(dev_g_idata);
-	cudaFree(dev_g_odata);
-}
-
-void sumGPUUnsignedChar(unsigned char *g_idata, unsigned char *g_odata, int size)
-{
-	int numInputElements = size;
-	int numOutputElements;
-	int threadsPerBlock = DIM;
-	unsigned char *dev_g_idata;
-	unsigned char *dev_g_odata;
-	cudaSetDevice(0);
-	cudaMalloc((unsigned char**)&dev_g_idata, size * sizeof(unsigned char));
-	cudaMalloc((unsigned char**)&dev_g_odata, size * sizeof(unsigned char));
-	cudaMemcpy(dev_g_idata, g_idata, size * sizeof(unsigned char), cudaMemcpyHostToDevice);
-
-	do
-	{
-		numOutputElements = numInputElements / (threadsPerBlock);
-		if (numInputElements % (threadsPerBlock))
-			numOutputElements++;
-
-		reductionUnsignedChar <<< numOutputElements, threadsPerBlock, DIM * sizeof(unsigned char) >>> (dev_g_idata, dev_g_odata, numInputElements);
-		numInputElements = numOutputElements;
-		if (numOutputElements > 1)
-			reductionUnsignedChar <<< numOutputElements, threadsPerBlock , DIM * sizeof(unsigned char)>>> (dev_g_odata, dev_g_idata, numInputElements);
-
-	} while (numOutputElements > 1);
-
-	cudaDeviceSynchronize();
-	cudaMemcpy(g_idata, dev_g_idata, size * sizeof(unsigned char), cudaMemcpyDeviceToHost);
-	cudaMemcpy(g_odata, dev_g_odata, size * sizeof(unsigned char), cudaMemcpyDeviceToHost);
-
-	cudaFree(dev_g_idata);
-	cudaFree(dev_g_odata);
 }
